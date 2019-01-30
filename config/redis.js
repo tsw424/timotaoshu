c9448aee9464a14cb3ab031ec5caf5a13921e929
tsw <tianshuxiaolei@163.com>
2019-01-30 17:54:21 +0800

    存储文件版本的保存

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,6 @@
 module.exports = {
-    RDS_PORT: 6379,            //服务器端口
-    RDS_HOST: '127.0.0.1',     //服务器ip
-    RDS_PWD: '',          //密码
+    RDS_PORT: 26379,            //服务器端口
+    RDS_HOST: '39.98.186.104',     //服务器ip
+    RDS_PWD: 'tianshuxiaoleiTSW424123321',          //密码
     RDS_OPTS: {},              //设置值       //是否把连接情况打印到文件里
 };
--- config/sql.js
+++ config/sql.js
@@ -1,9 +1,9 @@
 module.exports = {
-    host     : '127.0.0.1',
+    host     : '39.98.186.104',
     port     : '3306',
-    user     : 'root',
-    password : '',
-    database : 'timotaoshu',
+    user     : 'tsw',
+    password : '4241211',
+    database : 'timotaoshu2',
     multipleStatements : true,  //是否允许执行多条sql语句
     // insecureAuth: true,   //加入此项可解决此错误！！！  //使用旧（不安全）的连接方式去连接MySQL  远程连接
     // debug: true,            //是否把连接情况打印到文件里
